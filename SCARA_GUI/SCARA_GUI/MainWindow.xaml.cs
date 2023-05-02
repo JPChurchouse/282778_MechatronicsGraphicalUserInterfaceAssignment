6d7ff3fb5356eb83718b392ae54df9666df61de8
Jamie Churchouse <jamespeterchurchouse@gmail.com>
2023-05-02 13:01:14 +1200

    compiles, logging

---
+++ SCARA_GUI/SCARA_GUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Serilog;
+
+namespace SCARA_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitLog();
+        }
+
+        // Initalise logging
+        private void InitLog()
+        {
+            string timenow = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.File($"logs/{timenow}.txt")
+                .CreateLogger();
+            Log.Information("Started at time: " + timenow);
+        }
+    }
+}
